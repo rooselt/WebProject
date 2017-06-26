51f4008f841d3f60139811169f782d3e0865eec4
Usuario <Usuario@DANIELA-NB>
2017-06-25 21:37:12 -0300

    Add project files.

---
+++ WebProjectPA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebProjectPA.Startup))]
+namespace WebProjectPA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
